decac867032e558d4a052a767f3837f491416631
mizarion <zsgluschenko@edu.hse.ru>
2024-09-16 18:00:29 +0900

    Add unimplemented RedisMap class with tests

---
+++ src/main/java/org/example/RedisMap.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class RedisMap implements Map<String, String> {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public String get(Object key) {
+        return null;
+    }
+
+    @Override
+    public String put(String key, String value) {
+        return null;
+    }
+
+    @Override
+    public String remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends String> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<String> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<String> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<String, String>> entrySet() {
+        return null;
+    }
+}
---
+++ src/test/java/org/example/RedisMapTest.java
@@ -0,0 +1,95 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RedisMapTest {
+
+    private final RedisMap redisMap = new RedisMap();
+
+    @BeforeEach
+    public void setUp() {
+        redisMap.clear();
+    }
+
+    @Test
+    void isEmpty() {
+        Assertions.assertTrue(redisMap.isEmpty());
+        redisMap.put("key", "value");
+        Assertions.assertFalse(redisMap.isEmpty());
+    }
+
+    @Test
+    void containsKey() {
+        Assertions.assertFalse(redisMap.containsKey("key"));
+        redisMap.put("key", "value");
+        Assertions.assertTrue(redisMap.containsKey("key"));
+    }
+
+    @Test
+    void containsValue() {
+        Assertions.assertFalse(redisMap.containsValue("value"));
+        redisMap.put("key", "value");
+        Assertions.assertTrue(redisMap.containsValue("value"));
+    }
+
+    @Test
+    void get() {
+        Assertions.assertNull(redisMap.get("testget"));
+        redisMap.put("testget", "value");
+        Assertions.assertNotNull(redisMap.get("testget"));
+    }
+
+    @Test
+    void put() {
+        Assertions.assertEquals(0, redisMap.size());
+        redisMap.put("testput", "value");
+        Assertions.assertEquals(1, redisMap.size());
+    }
+
+    @Test
+    void remove() {
+        Assertions.assertEquals(0, redisMap.size());
+        redisMap.put("testremove", "value");
+        Assertions.assertEquals(1, redisMap.size());
+        redisMap.remove("testremove");
+        Assertions.assertEquals(0, redisMap.size());
+    }
+
+    @Test
+    void putAll() {
+        Assertions.assertEquals(0, redisMap.size());
+        Map<String, String> map = Map.of("key", "value");
+        redisMap.putAll(map);
+        Assertions.assertEquals(1, redisMap.size());
+    }
+
+    @Test
+    void clear() {
+        redisMap.put("key", "value");
+        Assertions.assertFalse(redisMap.isEmpty());
+        redisMap.clear();
+        Assertions.assertTrue(redisMap.isEmpty());
+    }
+
+    @Test
+    void clearEmptyMap() {
+        Assertions.assertTrue(redisMap.isEmpty());
+        redisMap.clear();
+        Assertions.assertTrue(redisMap.isEmpty());
+    }
+
+    @Test
+    void keySet() {
+        Assertions.assertEquals(0, redisMap.keySet().size());
+        redisMap.put("key", "value");
+        Assertions.assertEquals(1, redisMap.keySet().size());
+    }
+
+
+}
