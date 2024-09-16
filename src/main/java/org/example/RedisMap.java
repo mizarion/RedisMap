fc0800102673ae2519e8ff4b88036b7a2dcca254
mizarion <zsgluschenko@edu.hse.ru>
2024-09-16 19:13:34 +0900

    implement RedisMap

--- src/main/java/org/example/RedisMap.java
+++ src/main/java/org/example/RedisMap.java
@@ -1,68 +1,122 @@
 package org.example;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import redis.clients.jedis.Jedis;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RedisMap implements Map<String, String> {
 
+
+    private final Jedis jedis;
+    private final String keyPrefix;
+
+    public RedisMap() {
+        this.jedis = new Jedis();
+        this.keyPrefix = "prefix";
+    }
+
+    public RedisMap(String RedisKeyPrefix) {
+        this.jedis = new Jedis();
+        this.keyPrefix = RedisKeyPrefix;
+    }
+
+    public RedisMap(Jedis jedis, String RedisKeyPrefix) {
+        this.jedis = jedis;
+        this.keyPrefix = RedisKeyPrefix;
+    }
+
+    private String getKeyPrefix(String key) {
+        return keyPrefix + ":" + key;
+    }
+
     @Override
     public int size() {
-        return 0;
+        return jedis.keys(keyPrefix + ":*").size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        if (key instanceof String str) {
+            return jedis.exists(getKeyPrefix(str));
+        }
+        throw new IllegalArgumentException("Key must be a String");
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        if (value instanceof String str) {
+            return jedis.keys(keyPrefix + ":*").stream()
+                    .map(jedis::get)
+                    .toList()
+                    .contains(str);
+        }
+        throw new IllegalArgumentException("Value must be a String");
     }
 
     @Override
     public String get(Object key) {
-        return null;
+        if (key instanceof String str) {
+            return jedis.get(getKeyPrefix(str));
+        }
+        throw new IllegalArgumentException("Key must be a String");
     }
 
     @Override
     public String put(String key, String value) {
-        return null;
+        return jedis.set(getKeyPrefix(key), value);
     }
 
     @Override
     public String remove(Object key) {
-        return null;
+        if (key instanceof String str) {
+            String oldValue = jedis.get(getKeyPrefix(str));
+            jedis.del(getKeyPrefix(str));
+            return oldValue;
+        }
+        throw new IllegalArgumentException("Key must be a String");
     }
 
     @Override
     public void putAll(Map<? extends String, ? extends String> m) {
-
+        m.forEach(this::put);
     }
 
     @Override
     public void clear() {
-
+        Set<String> keys = jedis.keys(keyPrefix + ":*");
+        for (String key : keys) {
+            jedis.del(key);
+        }
     }
 
     @Override
     public Set<String> keySet() {
-        return null;
+        return jedis.keys(keyPrefix + ":*").stream()
+                .map(keyPrefix -> keyPrefix.replace(keyPrefix + ":", ""))
+                .collect(Collectors.toSet());
     }
 
     @Override
     public Collection<String> values() {
-        return null;
+        return jedis.keys(keyPrefix + ":*").stream()
+                .map(jedis::get)
+                .toList();
     }
 
     @Override
     public Set<Entry<String, String>> entrySet() {
-        return null;
+        Set<String> keys = jedis.keys(keyPrefix + ":*");
+        Set<Entry<String, String>> entrySet = new HashSet<>();
+        for (String key : keys) {
+            String actualKey = key.replace(keyPrefix + ":", "");
+            entrySet.add(new AbstractMap.SimpleEntry<>(actualKey, jedis.get(key)));
+        }
+        return entrySet;
     }
 }
